2956a41bc225881706040b3981931217250d27a1
aivve <maksymivv@gmail.com>
2018-05-01 04:16:42 +0300

    CryptoNight Heavy
    adaptation from SumoKoin

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -585,26 +585,32 @@ namespace CryptoNote {
 		return next_difficulty;
 	}	
 
-	bool Currency::checkProofOfWorkV1(Crypto::cn_context& context, const Block& block, difficulty_type currentDiffic,
+	bool Currency::checkProofOfWorkV1(const Block& block, difficulty_type currentDiffic,
 		Crypto::Hash& proofOfWork) const {
+
+		cn_pow_hash_v2 m_pow_ctx;
+
 		if (BLOCK_MAJOR_VERSION_1 != block.majorVersion) {
 			return false;
 		}
 
-		if (!get_block_longhash(context, block, proofOfWork)) {
+		if (!get_block_longhash(m_pow_ctx, block, proofOfWork)) {
 			return false;
 		}
 
 		return check_hash(proofOfWork, currentDiffic);
 	}
 
-	bool Currency::checkProofOfWorkV2(Crypto::cn_context& context, const Block& block, difficulty_type currentDiffic,
+	bool Currency::checkProofOfWorkV2(const Block& block, difficulty_type currentDiffic,
 		Crypto::Hash& proofOfWork) const {
+
+		cn_pow_hash_v2 m_pow_ctx;
+
 		if (block.majorVersion < BLOCK_MAJOR_VERSION_2) {
 			return false;
 		}
 
-		if (!get_block_longhash(context, block, proofOfWork)) {
+		if (!get_block_longhash(m_pow_ctx, block, proofOfWork)) {
 			return false;
 		}
 
@@ -639,15 +645,15 @@ namespace CryptoNote {
 		return true;
 	}
 
-	bool Currency::checkProofOfWork(Crypto::cn_context& context, const Block& block, difficulty_type currentDiffic, Crypto::Hash& proofOfWork) const {
+	bool Currency::checkProofOfWork(const Block& block, difficulty_type currentDiffic, Crypto::Hash& proofOfWork) const {
 		switch (block.majorVersion) {
 		case BLOCK_MAJOR_VERSION_1:
-			return checkProofOfWorkV1(context, block, currentDiffic, proofOfWork);
+			return checkProofOfWorkV1(block, currentDiffic, proofOfWork);
 
 		case BLOCK_MAJOR_VERSION_2:
 		case BLOCK_MAJOR_VERSION_3:
 		case BLOCK_MAJOR_VERSION_4:
-			return checkProofOfWorkV2(context, block, currentDiffic, proofOfWork);
+			return checkProofOfWorkV2(block, currentDiffic, proofOfWork);
 		}
 
 		logger(ERROR, BRIGHT_RED) << "Unknown block major version: " << block.majorVersion << "." << block.minorVersion;
