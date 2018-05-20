1a734041ff284cea892113d1e5957efde9a75a46
aiwe <maksymivv@gmail.com>
2018-05-20 17:55:03 +0300

    Parent block major version 1

--- src/CryptoNoteCore/Core.cpp
+++ src/CryptoNoteCore/Core.cpp
@@ -427,7 +427,7 @@ bool core::get_block_template(Block& b, const AccountPublicAddress& adr, difficu
       }
 
 	  if (b.majorVersion >= BLOCK_MAJOR_VERSION_4) {
-		  b.parentBlock.majorVersion = b.majorVersion;
+		  b.parentBlock.majorVersion = BLOCK_MAJOR_VERSION_1;
 		  b.parentBlock.minorVersion = BLOCK_MINOR_VERSION_1;
 	  }
 	  else {
