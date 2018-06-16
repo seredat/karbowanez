665a4ddaf78e62d3acb8c0750348636f17d9c06d
Aiwe <maksymivv@gmail.com>
2018-06-16 11:47:38 +0300

    DnsTools minor Linux fixes

--- src/Common/DnsTools.cpp
+++ src/Common/DnsTools.cpp
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Karbo. If not, see <http://www.gnu.org/licenses/>.
 
-#pragma once
-
 #include <condition_variable>
 #include <future>
 #include <memory>
