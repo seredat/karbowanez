0ba448f4cb411023d7092a954596b3e95137c9e3
aiwe <maksymivv@gmail.com>
2018-06-26 22:40:41 +0300

    Remove parent block, merged mining tag

--- src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.cpp
@@ -466,7 +466,7 @@ bool get_block_hash(const Block& b, Hash& res) {
   }
 
   // The header of block version 1 differs from headers of blocks starting from v.2
-  if (BLOCK_MAJOR_VERSION_2 <= b.majorVersion) {
+  if (BLOCK_MAJOR_VERSION_2 == b.majorVersion || BLOCK_MAJOR_VERSION_3 == b.majorVersion) {
     BinaryArray parent_blob;
     auto serializer = makeParentBlockSerializer(b, true, false);
     if (!toBinaryArray(serializer, parent_blob))
