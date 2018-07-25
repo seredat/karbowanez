147c9c943db751e03dbfa0c14fefd54b1486fe7d
aiwe <maksymivv@gmail.com>
2018-07-25 20:32:12 +0300

    Hardfork v4 height
    set to 266000

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -91,7 +91,7 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 
 const uint32_t UPGRADE_HEIGHT_V2                             = 60000;
 const uint32_t UPGRADE_HEIGHT_V3                             = 216000;
-const uint32_t UPGRADE_HEIGHT_V4                             = 4294967294;
+const uint32_t UPGRADE_HEIGHT_V4                             = 266000;
 const unsigned UPGRADE_VOTING_THRESHOLD                      = 90; // percent
 const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
 const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  // blocks
