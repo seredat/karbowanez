e70bf22bbca2bfe9b36da56e37710c47a33f635f
aiwe <maksymivv@gmail.com>
2018-08-15 23:01:46 +0300

    Total supply and block major version in /getinfo

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -418,6 +418,10 @@ bool RpcServer::on_get_info(const COMMAND_RPC_GET_INFO::request& req, COMMAND_RP
   res.min_tx_fee = m_core.getMinimalFee();
   res.readable_tx_fee = m_core.currency().formatAmount(m_core.getMinimalFee());
   res.start_time = (uint64_t)m_core.getStartTime();
+  res.block_major_version = m_core.getCurrentBlockMajorVersion();
+  // that large uint64_t number is unsafe in JavaScript environment and therefore as a JSON value so we display it as a formatted string
+  res.already_generated_coins = m_core.currency().formatAmount(m_core.getTotalGeneratedAmount());
+
   res.status = CORE_RPC_STATUS_OK;
   return true;
 }
