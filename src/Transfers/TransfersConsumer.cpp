d18c4d42103b811061cf467bb8a87ad46af85090
aiwe <maksymivv@gmail.com>
2018-10-03 15:19:34 +0300

    Fix burning bug fix
    ensure it catch duplicate outputs if first one is not duplicate
    
    Thanks hatoyakohane from BBSCoin

--- src/Transfers/TransfersConsumer.cpp
+++ src/Transfers/TransfersConsumer.cpp
@@ -394,6 +394,7 @@ std::error_code createTransfers(
   std::vector<TransactionOutputInformationIn>& transfers) {
 
   auto txPubKey = tx.getTransactionPublicKey();
+  std::vector<PublicKey> temp_keys;
 
   for (auto idx : outputs) {
 
@@ -441,10 +442,7 @@ std::error_code createTransfers(
 				  return std::error_code();
 			  }
 		  }
-	  
-		  std::vector<PublicKey> temp_keys;
 		  temp_keys.push_back(out.key);
-		  public_keys_seen.insert(std::make_pair(tx.getTransactionHash(), temp_keys));
 	  }
 
       info.amount = amount;
@@ -477,6 +475,7 @@ std::error_code createTransfers(
 
     transfers.push_back(info);
   }
+  public_keys_seen.insert(std::make_pair(tx.getTransactionHash(), temp_keys));
 
   return std::error_code();
 }
