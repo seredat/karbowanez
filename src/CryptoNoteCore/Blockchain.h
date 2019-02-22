9e0667f71aa14c78dbc9fc30dbf06ed6227076b1
aiwe <maksymivv@gmail.com>
2019-02-22 22:02:53 +0200

    Get average cumulative difficulty

--- src/CryptoNoteCore/Blockchain.h
+++ src/CryptoNoteCore/Blockchain.h
@@ -90,6 +90,8 @@ namespace CryptoNote {
     Crypto::Hash getTailId();
     Crypto::Hash getTailId(uint32_t& height);
     difficulty_type getDifficultyForNextBlock();
+	difficulty_type getAvgCumulativeDifficulty(uint32_t& height);
+	difficulty_type getAvgDifficulty(uint32_t height, size_t window);
 	uint64_t getBlockTimestamp(uint32_t height);
 	uint64_t getMinimalFee(uint32_t height);
     uint64_t getCoinsInCirculation();
@@ -123,7 +125,6 @@ namespace CryptoNote {
     bool getTransactionIdsByPaymentId(const Crypto::Hash& paymentId, std::vector<Crypto::Hash>& transactionHashes);
     bool isBlockInMainChain(const Crypto::Hash& blockId);
     bool isInCheckpointZone(const uint32_t height);
-    uint64_t getAvgDifficultyForHeight(uint32_t height, size_t window);
 
     template<class visitor_t> bool scanOutputKeysForIndexes(const KeyInput& tx_in_to_key, visitor_t& vis, uint32_t* pmax_related_block_height = NULL);
 
