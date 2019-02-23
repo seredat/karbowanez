d848c9b0a3300fe3a56f773f194d87589ddecdb8
aiwe <maksymivv@gmail.com>
2019-02-23 11:43:41 +0200

    Remove commented out

--- src/crypto/rainforest.h
+++ src/crypto/rainforest.h
@@ -14,29 +14,10 @@
 // CRC32 instruction is used a lot as it is extremely fast on low-power ARM
 // chips and allows such devices to rival high-end PCs mining performance.
 //
-// Tests on various devices have shown the following performance :
-// +--------------------------------------------------------------------------+
-// | CPU/GPU       Clock Threads Full hash  Nonce scan  Watts   Cost          |
-// |               (MHz)         (80 bytes) (4 bytes)   total                 |
-// | Core i7-6700k  4000      8   390 kH/s  1642 kH/s     200  ~$350+PC       |
-// | Radeon RX560   1300   1024  1100 kH/s  1650 kH/s     300  ~$180+PC       |
-// | RK3368 (8*A53) 1416      8   534 kH/s  1582 kH/s       6   $60 (Geekbox) |
-// +--------------------------------------------------------------------------+
-//
-// Build instructions on Ubuntu 16.04 :
-//   - on x86:   use gcc -march=native or -maes to enable AES-NI
-//   - on ARMv8: use gcc -march=native or -march=armv8-a+crypto+crc to enable
-//               CRC32 and AES extensions.
-//
-// Note: always use the same options to build all files!
-//
 
 #ifndef RAINFOREST
 #define RAINFOREST
 
-//#ifdef __cplusplus
-//extern "C" {
-//#endif
 #define __attribute__(X)
 #include <stdint.h>
 
@@ -72,7 +53,3 @@ void rf256_hash(void *out, const void *in, size_t len);
 void rf256_hash2(void *out, const void *in, size_t len, uint32_t seed);
 
 #endif
-
-//#ifdef __cplusplus
-//}
-//#endif
