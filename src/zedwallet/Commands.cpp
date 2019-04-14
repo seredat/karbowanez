2ad70ad1b79c0dd93e7c44dd25bc3ef6426df948
aiwe <maksymivv@gmail.com>
2019-04-14 11:44:42 +0300

    Remove unused command

--- src/zedwallet/Commands.cpp
+++ src/zedwallet/Commands.cpp
@@ -50,7 +50,7 @@ std::vector<AdvancedCommand> allCommands()
         AdvancedCommand("ab_list", "List everyone in your address book", true, true),
         AdvancedCommand("ab_send", "Send " + WalletConfig::ticker + " to someone in your address book", false, true),
         AdvancedCommand("change_password", "Change your wallet password", true, true),
-        AdvancedCommand("make_integrated_address", "Make a combined address + payment ID", true, true),
+        //AdvancedCommand("make_integrated_address", "Make a combined address + payment ID", true, true),
         AdvancedCommand("incoming_transfers", "Show incoming transfers", true, true),
         AdvancedCommand("list_transfers", "Show all transfers", false, true),
         AdvancedCommand("optimize", "Optimize your wallet to send large amounts", false, true),
