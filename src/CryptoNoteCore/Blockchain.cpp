7bf922182ba2066fd89980d501b17ed6579ff4b0
Luke <luke@clashproject.com>
2019-05-13 21:20:02 +0200

    Add iterations according to the original BloDHa paper
    (in KIP-001 there weren't iterations)

--- src/CryptoNoteCore/Blockchain.cpp
+++ src/CryptoNoteCore/Blockchain.cpp
@@ -1254,52 +1254,49 @@ bool Blockchain::get_block_long_hash(Crypto::cn_context &context, const Block& b
     return false;
   }
 
-  // Phase 1
-
+#define ITER 128
   Crypto::Hash hash_1, hash_2;
 
-  // Hashing the current blockdata (preprocessing it)
-  cn_fast_hash(pot.data(), pot.size(), hash_1);
-  
-  // Phase 2
-
-  // Get the corresponding 8 blocks from blockchain based on preparatory hash_1
-  // and throw them into the pot too
   {
     std::lock_guard<decltype(m_blockchain_lock)> lk(m_blockchain_lock);
 
     uint32_t currentHeight = boost::get<BaseInput>(b.baseTransaction.inputs[0]).blockIndex;
     uint32_t maxHeight = std::min<uint32_t>(m_blocks.size() - 1, currentHeight - 1 - m_currency.minedMoneyUnlockWindow());
 
-    for (uint8_t i = 1; i <= 8; i++) {
-      uint8_t chunk[4] = {
-        hash_1.data[i * 4 - 4], 
-        hash_1.data[i * 4 - 3], 
-        hash_1.data[i * 4 - 2], 
-        hash_1.data[i * 4 - 1]
-      };
-    
-      uint32_t n = (chunk[0] << 24) |
-                   (chunk[1] << 16) |
-                   (chunk[2] << 8)  |
-                   (chunk[3]);
+    for (uint32_t i = 0; i < ITER; i++) {
 
-      uint32_t height_i = n % maxHeight;
+      cn_fast_hash(pot.data(), pot.size(), hash_1);
 
-      Block bi = m_blocks[height_i].bl;
+      for (uint8_t j = 1; j <= 8; j++) {
+        uint8_t chunk[4] = {
+          hash_1.data[j * 4 - 4], 
+          hash_1.data[j * 4 - 3], 
+          hash_1.data[j * 4 - 2], 
+          hash_1.data[j * 4 - 1]
+        };
     
-      BinaryArray ba;
-      if (!get_block_hashing_blob(bi, ba)) {
-        logger(ERROR, BRIGHT_RED) << "Failed to get_block_hashing_blob of additional block " 
-                                  << i << " at height " << height_i;
-        return false;
+        uint32_t n = (chunk[0] << 24) |
+                     (chunk[1] << 16) |
+                     (chunk[2] << 8)  |
+                     (chunk[3]);
+
+        uint32_t height_j = n % maxHeight;
+
+        const Block& bi = m_blocks[height_j].bl;
+
+        BinaryArray ba;
+        if (!get_block_hashing_blob(bi, ba)) {
+          logger(ERROR, BRIGHT_RED) << "Failed to get_block_hashing_blob of additional block " 
+                                    << j << " at height " << height_j;
+          return false;
+        }
+
+        pot.insert(std::end(pot), std::begin(ba), std::end(ba));
       }
 
-      pot.insert(std::end(pot), std::begin(ba), std::end(ba));
     }
-  }
 
-  // Phase 3
+  }
 
   // stir the pot - hashing the 1 + 8 blocks as one continuous data
   if (!Crypto::y_slow_hash(pot.data(), pot.size(), hash_1, hash_2)) {
