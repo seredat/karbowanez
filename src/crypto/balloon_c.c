b2d3f06306fdb235b1033791705bb917e8390387
ClashLuke <luke@clashproject.com>
2019-06-28 00:44:08 +0200

    Changed clash-balloon config

--- src/crypto/balloon_c.c
+++ src/crypto/balloon_c.c
@@ -5,8 +5,8 @@
 
 #include <stdint.h>
 
-#define SIZE (1<<21) // Bytes, stored in cache
-#define ITER (1<<20) // Iterations to move over the cache
+#define SIZE (1<<21)  // Bytes, stored in cache
+#define ITER (1<<16)  // Iterations to move over the cache
 #define INPUT_SIZE  4 // In  8-byte blocks, maximum is 16
 #define OUTPUT_SIZE 2 // In 16-byte blocks, maximum  is 4
 
