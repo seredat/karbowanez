7337d866753771f43bb4c79aa515d2474e75d886
aiwe <maksymivv@gmail.com>
2019-06-27 23:13:24 +0300

    Fix build on msvc

--- src/crypto/balloon_c.h
+++ src/crypto/balloon_c.h
@@ -2,4 +2,17 @@
 //
 // Please see the included LICENSE file for more information.
 
+#ifndef BALLOON_C_H
+#define BALLOON_C_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void balloon(uint8_t* data, uint8_t* out);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
