efb827d414c7429e7bd91038530c859117109fd6
aiwe <maksymivv@gmail.com>
2019-06-28 14:18:38 +0300

    Define balloons params

--- src/crypto/balloon_g.c
+++ src/crypto/balloon_g.c
@@ -27,6 +27,10 @@
 #include <stdint.h>
 #include <string.h>
 
+#define S_COST 64
+#define T_COST 2
+#define DELTA  3
+
 inline uint64_t u8tou64(uint8_t const* u8){
 	uint64_t u64;
 	memcpy(&u64, u8, sizeof(u64));
@@ -35,12 +39,8 @@ inline uint64_t u8tou64(uint8_t const* u8){
 
 void balloon_g(const unsigned char* input, char* output, int length, const unsigned char* salt, int salt_length)
 {
-	const uint64_t s_cost = 64;
-	const uint64_t t_cost = 2;
-	const int delta = 3;
-	
 	hashState ctx_groestl;
-	uint8_t blocks[64][64];
+	uint8_t blocks[S_COST][64];
 	
 	// Step 1: Expand input into buffer
 	uint64_t cnt = 0;
@@ -51,7 +51,7 @@ void balloon_g(const unsigned char* input, char* output, int length, const unsig
   Final(&ctx_groestl, blocks[0]);
 	cnt++;
 
-	for (int m = 1; m < s_cost; m++) {
+	for (int m = 1; m < S_COST; m++) {
     Update(&ctx_groestl, (uint8_t *)&cnt, sizeof((uint8_t *)&cnt));
     Update(&ctx_groestl, blocks[m - 1], 64);
 		Final(&ctx_groestl, blocks[m]);
@@ -59,16 +59,16 @@ void balloon_g(const unsigned char* input, char* output, int length, const unsig
 	}
 
 	// Step 2: Mix buffer contents
-	for (uint64_t t = 0; t < t_cost; t++) {
-		for (uint64_t m = 0; m < s_cost; m++) {
+	for (uint64_t t = 0; t < T_COST; t++) {
+		for (uint64_t m = 0; m < S_COST; m++) {
 			// Step 2a: Hash last and current blocks
       Update(&ctx_groestl, (uint8_t *)&cnt, sizeof((uint8_t *)&cnt));
-      Update(&ctx_groestl, blocks[(m - 1) % s_cost], 64);
+      Update(&ctx_groestl, blocks[(m - 1) % S_COST], 64);
       Update(&ctx_groestl, blocks[m], 64);
 			Final(&ctx_groestl, blocks[m]);
 			cnt++;
 
-			for (uint64_t i = 0; i < delta; i++) {
+			for (uint64_t i = 0; i < DELTA; i++) {
 				uint8_t index[64];
         Update(&ctx_groestl, (uint8_t *)&t, sizeof((uint8_t *)&t));
         Update(&ctx_groestl, (uint8_t *)&cnt, sizeof((uint8_t *)&cnt));
@@ -78,7 +78,7 @@ void balloon_g(const unsigned char* input, char* output, int length, const unsig
 				Final(&ctx_groestl, index);
 				cnt++;
 
-				uint64_t other = u8tou64(index) % s_cost;
+				uint64_t other = u8tou64(index) % S_COST;
 				cnt++;
 
         Update(&ctx_groestl, (uint8_t *)&cnt, sizeof((uint8_t *)&cnt));
@@ -90,5 +90,5 @@ void balloon_g(const unsigned char* input, char* output, int length, const unsig
 		}
 	}
 	
-	memcpy(output, blocks[s_cost - 1], 32);
+	memcpy(output, blocks[S_COST - 1], 32);
 }
