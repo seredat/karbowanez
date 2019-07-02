76214ca718bcb81d2087365d847f9600a9acdd87
aiwe <maksymivv@gmail.com>
2019-07-02 11:01:35 +0300

    Tidyup Crypto

--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -70,7 +70,9 @@ union cn_slow_hash_state
 #include <intrin.h>
 #include <windows.h>
 #define STATIC
+#if !defined(INLINE)
 #define INLINE __inline
+#endif
 #if !defined(RDATA_ALIGN16)
 #define RDATA_ALIGN16 __declspec(align(16))
 #endif
