71544524895af8f92749dfb423bb9af44c8310b7
aiwe <maksymivv@gmail.com>
2019-09-23 17:53:39 +0300

    Check locked amounts in reserve proof RPC method

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -1774,10 +1774,13 @@ bool RpcServer::on_check_reserve_proof(const K_COMMAND_RPC_CHECK_RESERVE_PROOF::
 	// check spent status
 	res.total = 0;
 	res.spent = 0;
+	res.locked = 0;
 	for (size_t i = 0; i < proofs.size(); ++i) {
 		const reserve_proof_entry& proof = proofs[i];
 
 		CryptoNote::TransactionPrefix tx = *static_cast<const TransactionPrefix*>(&transactions[i]);
+    
+		bool unlocked = m_core.is_tx_spendtime_unlocked(tx.unlockTime, req.height);
 
 		if (proof.index_in_tx >= tx.outputs.size()) {
 			throw JsonRpc::JsonRpcError{ CORE_RPC_ERROR_CODE_INTERNAL_ERROR, "index_in_tx is out of bound" };
@@ -1814,6 +1817,10 @@ bool RpcServer::on_check_reserve_proof(const K_COMMAND_RPC_CHECK_RESERVE_PROOF::
 			if (pubkey == out_key.key) {
 				uint64_t amount = tx.outputs[proof.index_in_tx].amount;
 				res.total += amount;
+
+        if (!unlocked) {
+          res.locked += amount;
+        }
 
         if (req.height != 0) {
           if (m_core.is_key_image_spent(proof.key_image, req.height)) {
