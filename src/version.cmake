b52a1538950c435dca1410a1459096fbc4bb2eb8
aiwe <maksymivv@gmail.com>
2019-12-16 11:05:10 +0200

    Change cmake files responsible for versioning

--- src/version.cmake
+++ src/version.cmake
@@ -1,11 +1,37 @@
-execute_process(COMMAND "${GIT}" describe --dirty --match "v*" RESULT_VARIABLE RET OUTPUT_VARIABLE DESCRIPTION OUTPUT_STRIP_TRAILING_WHITESPACE)
-if(RET)
-    message(WARNING "Cannot determine current revision. Make sure that you are building either from a Git working tree or from a source archive.")
-    set(VERSION "${COMMIT}")
+# Read project version from git tag.
+find_package(Git)
+message(STATUS "Found Git: ${GIT_FOUND}")
+if(GIT_FOUND)
+  execute_process(
+    COMMAND "${GIT_EXECUTABLE}" describe --tags --abbrev=0 --match "v*"
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    RESULT_VARIABLE PROJECT_GIT_RESULT
+    OUTPUT_VARIABLE PROJECT_GIT_TAG
+    ERROR_VARIABLE PROJECT_GIT_ERROR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE)
+  if(NOT PROJECT_GIT_RESULT EQUAL 0)
+    set(PROJECT_GIT_TAG "0.0.0")
+    message(WARNING "Failed to execute Git: ${PROJECT_GIT_ERROR}")
+  else()
+    string(REPLACE "v." "" PROJECT_GIT_TAG ${PROJECT_GIT_TAG})
+    message(STATUS "Git tag: ${PROJECT_GIT_TAG}")
+  endif()
+	
+  execute_process(
+    COMMAND "${GIT_EXECUTABLE}" rev-parse --short HEAD
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    RESULT_VARIABLE PROJECT_GIT_RESULT
+    OUTPUT_VARIABLE PROJECT_GIT_COMMIT_ID
+    ERROR_VARIABLE PROJECT_GIT_ERROR
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    ERROR_STRIP_TRAILING_WHITESPACE)
+  if(NOT PROJECT_GIT_RESULT EQUAL 0)
+    set(PROJECT_GIT_COMMIT_ID "unknown")
+    message(WARNING "Failed to execute Git: ${PROJECT_GIT_ERROR}")
     configure_file("${CMAKE_CURRENT_SOURCE_DIR}/src/version.h.in" "${TO}")
-else()
-    string(REGEX MATCH "([0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f][0-9a-f])?(-dirty)? $" COMMIT "${DESCRIPTION} ")
-    string(STRIP "${COMMIT}" COMMIT)
-    set(VERSION "${COMMIT}")
-    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/src/version.h.in" "${TO}")
+  else()
+    message(STATUS "Git commit ID: ${PROJECT_GIT_COMMIT_ID}")
+    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/src/version.h.in" "${TO}")		
+  endif()
 endif()
