0251ac452afb29e28868edbda1cc03a54fdbfe24
aiwe <maksymivv@gmail.com>
2020-04-02 21:14:57 +0300

    The difference is not so significant, reverse SMA

--- src/CryptoNoteCore/Blockchain.h
+++ src/CryptoNoteCore/Blockchain.h
@@ -94,7 +94,6 @@ namespace CryptoNote {
     difficulty_type getDifficultyForNextBlock();
     difficulty_type getAvgDifficulty(uint32_t height);
     difficulty_type getAvgDifficulty(uint32_t height, size_t window);
-    difficulty_type getSMADifficulty(uint32_t height, size_t window);
     uint64_t getBlockTimestamp(uint32_t height);
     uint64_t getMinimalFee(uint32_t height);
     uint64_t getCoinsInCirculation();
