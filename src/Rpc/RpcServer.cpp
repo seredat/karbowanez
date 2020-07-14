7920463229ec4d91f837516d1599013fe27ddfb1
aiwe <maksymivv@gmail.com>
2020-07-14 14:43:44 +0300

    Added max_cumulative_block_size to /getinfo

--- src/Rpc/RpcServer.cpp
+++ src/Rpc/RpcServer.cpp
@@ -970,18 +970,17 @@ bool RpcServer::on_get_info(const COMMAND_RPC_GET_INFO::request& req, COMMAND_RP
   res.contact = m_contact_info.empty() ? std::string() : m_contact_info;
   res.min_fee = m_core.getMinimalFee();
   res.start_time = (uint64_t)m_core.getStartTime();
-
   uint64_t alreadyGeneratedCoins = m_core.getTotalGeneratedAmount();
   // that large uint64_t number is unsafe in JavaScript environment and therefore as a JSON value so we display it as a formatted string
   res.already_generated_coins = m_core.currency().formatAmount(alreadyGeneratedCoins);
   res.block_major_version = m_core.getCurrentBlockMajorVersion();
   uint64_t nextReward = m_core.currency().calculateReward(alreadyGeneratedCoins);
   res.next_reward = nextReward;
-  
   if (!m_core.getBlockCumulativeDifficulty(res.height - 1, res.cumulative_difficulty)) {
     throw JsonRpc::JsonRpcError{
       CORE_RPC_ERROR_CODE_INTERNAL_ERROR, "Internal error: can't get last cumulative difficulty." };
   }
+  res.max_cumulative_block_size = (uint64_t)m_core.currency().maxBlockCumulativeSize(res.height);
 
   res.status = CORE_RPC_STATUS_OK;
   return true;
